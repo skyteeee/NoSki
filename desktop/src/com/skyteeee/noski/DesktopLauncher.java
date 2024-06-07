f9e9ab9c3525a16b47528522c22d9a5db62b1c04
Skyteeee <spusi.vapus@gmail.com>
2024-06-06 20:02:28 -0500

    initial commit. generated project template for libgdx

---
+++ desktop/src/com/skyteeee/noski/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.skyteeee.noski;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.skyteeee.noski.NoSkiGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("NoSki");
+		new Lwjgl3Application(new NoSkiGame(), config);
+	}
+}
