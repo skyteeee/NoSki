6f1b5354714ecd1374684b0dd2a69adb8c96bb4e
Skyteeee <spusi.vapus@gmail.com>
2024-09-13 20:50:08 -0500

    added texture atlas, button texture, added true type fonts, added pretty colors.

--- core/src/com/skyteeee/noski/NoSkiGame.java
+++ core/src/com/skyteeee/noski/NoSkiGame.java
@@ -1,33 +1,65 @@
 package com.skyteeee.noski;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.skyteeee.noski.screens.MainMenu;
 
 public class NoSkiGame extends Game {
 	public SpriteBatch batch;
-	public BitmapFont font;
-	Texture img;
-	
+	public BitmapFont mainFont;
+	public BitmapFont titleFont;
+	public BitmapFont smallFont;
+	public TextureAtlas uiAtlas;
+	public static Color colorBG = new Color(0x6a9a6eff);
+	public static Color colorTextOnButton = new Color(0x274e13ff);
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		font = new BitmapFont();
-		font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-		font.getData().setScale(5);
-		font.setColor(0,0,0,1);
-		img = new Texture("badlogic.jpg");
+		loadFonts();
+		loadTextures();
 		setScreen(new MainMenu(this));
+	}
+
+	private void loadTextures() {
+		uiAtlas = new TextureAtlas(Gdx.files.internal("ui/ui.atlas"));
+
+	}
+
+	private void loadFonts() {
+
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/regular.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameter.size = 32;
+		mainFont = generator.generateFont(parameter);
+		mainFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+		mainFont.setColor(colorTextOnButton);
+
+		parameter.size = 24;
+		smallFont = generator.generateFont(parameter);
+		smallFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+		smallFont.setColor(colorTextOnButton);
+
+		generator.dispose();
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/semibold.ttf"));
+		parameter.size = 48;
+		titleFont = generator.generateFont(parameter);
+		titleFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+		titleFont.setColor(colorTextOnButton);
+		generator.dispose();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		font.dispose();
-		img.dispose();
+		mainFont.dispose();
+		titleFont.dispose();
+		smallFont.dispose();
 	}
 }
--- core/src/com/skyteeee/noski/screens/MainMenu.java
+++ core/src/com/skyteeee/noski/screens/MainMenu.java
@@ -1,20 +1,28 @@
 package com.skyteeee.noski.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.skyteeee.noski.NoSkiGame;
 
 public class MainMenu implements Screen {
     final NoSkiGame game;
     OrthographicCamera camera;
+    ExtendViewport viewport;
+    NinePatch button;
 
     public MainMenu(final NoSkiGame game) {
         this.game = game;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1600, 900);
+        viewport = new ExtendViewport(1600,900, camera);
+
+        button = game.uiAtlas.createPatch("NoSkiButton");
+
     }
     @Override
     public void show() {
@@ -23,17 +31,19 @@ public class MainMenu implements Screen {
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0.7f,1f,0.7f, 1);
-        camera.update();
+        ScreenUtils.clear(NoSkiGame.colorBG);
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-        game.font.draw(game.batch,"Hello world!", 400, 700, 800f, Align.center, false);
+        game.titleFont.draw(game.batch,"Hello world!", 400, 700, 800f, Align.center, false);
+        button.draw(game.batch, 600, 500, 400, 100);
+        game.titleFont.draw(game.batch, "start", 600, 570, 400, Align.center, false);
         game.batch.end();
+
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width,height, true);
     }
 
     @Override
